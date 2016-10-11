b5f409e19a32d774133fddaa7c3736a81d611dc7
rodhall85 <rodhall@hotmail.co.uk>
2016-10-11 10:48:51 +0100

    #11 Change Node App to be ES5 as DockerCloud/AWS doesn't appear to support it yet

--- server/app.js
+++ server/app.js
@@ -1,63 +1,64 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const mongoUtil = require('./mongoUtil')
-const bodyParser = require('body-parser')
-const app = express()
+var express = require('express');
+var mongoUtil = require('./mongoUtil');
+var bodyParser = require('body-parser');
+var app = express();
 
-mongoUtil.connect()
+mongoUtil.connect();
 
-app.use(express.static(__dirname + '/../client'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/clubs/get/:clubName', (request, response) => {
-  console.log('bingo')
-  const clubName = request.params.clubName
-  if (!clubName) { response.sendStatus(400) }
+app.get('/clubs/get/:clubName', function (request, response) {
+  console.log('bingo');
+  var clubName = request.params.clubName;
+  if (!clubName) {
+    response.sendStatus(400);
+  }
 
-  const clubs = mongoUtil.clubs()
-  let existingClub = clubs.find({clubName: clubName})
+  var clubs = mongoUtil.clubs();
+  var existingClub = clubs.find({ clubName: clubName });
 
-  if (!existingClub) { response.sendStatus(404) }
- console.log(existingClub)
-  response.sendStatus(200)
-  response.body = existingClub
-})
+  if (!existingClub) {
+    response.sendStatus(404);
+  }
+  console.log(existingClub);
+  response.sendStatus(200);
+  response.body = existingClub;
+});
 
-app.post('/clubs/post', (request, response) => {
-  const club = request.body.club
-  console.log('inserting bluc')
-  console.log(club)
-  const clubs = mongoUtil.clubs()
-  clubs.insert(club).then(data => {
+app.post('/clubs/post', function (request, response) {
+  var club = request.body.club;
+  console.log('inserting bluc');
+  console.log(club);
+  var clubs = mongoUtil.clubs();
+  clubs.insert(club).then(function (data) {
     if (data.insertedCount === 1) {
-      response.sendStatus(201)
-      console.log(201)
+      response.sendStatus(201);
+      console.log(201);
+    } else {
+      console.log(500);
+      response.sendStatus(500);
     }
-    else {
-      console.log(500)
-      response.sendStatus(500)
-    }
-  }, err => {
-    console.log(err)
-  })
+  }, function (err) {
+    console.log(err);
+  });
 
-  app.put('/clubs/put', (request, response) => {
-    const club = request.body.club
-    const clubs = mongoUtil.clubs()
-    clubs.update(
-      { _id: club._id },
-      club
-    ).then(data => {
+  app.put('/clubs/put', function (request, response) {
+    var club = request.body.club;
+    var clubs = mongoUtil.clubs();
+    clubs.update({ _id: club._id }, club).then(function (data) {
       if (data.modifiedCount === 1) {
-        response.sendStatus(200)
+        response.sendStatus(200);
+      } else {
+        response.sendStatus(500);
       }
-      else {
-        response.sendStatus(500)
-      }
-    })
-  })
-})
+    });
+  });
+});
 
-app.listen(8181, () => console.log('Listening on port 8181')) // eslint-disable-line no-console
+app.listen(8181, function () {
+  return console.log('Listening on port 8181');
+}); // eslint-disable-line no-console
